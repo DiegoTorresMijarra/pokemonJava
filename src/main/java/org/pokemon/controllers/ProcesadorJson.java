a60e0e4f13657338f7470bb4468d4dbbb73821df
DiegoTorresMijarra <diegotorresmijarra@gmail.com>
2023-09-29 18:30:05 +0200

    feat(controllers)<ProcesadorJson>
    Procesa casi todo, faltan algunos atributos

--- src/main/java/org/pokemon/controllers/ProcesadorJson.java
+++ src/main/java/org/pokemon/controllers/ProcesadorJson.java
@@ -12,20 +12,26 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 @Getter
 public class ProcesadorJson {
     private static ProcesadorJson instancia;
     private Pokedex pokedex;
     private static final String dataPath= Paths.get("").toAbsolutePath().toString()+ File.separator + "data";
-    private static final String jsonFile="pokemon1.json";
+    private static final String jsonFile="pokemon.json";
     public static ProcesadorJson getInstancia() {
         if (instancia == null) {
             instancia = new ProcesadorJson();
         }
         return instancia;
+    }
+
+    public Pokedex getPokedex() {
+        return pokedex;
     }
 
     ProcesadorJson(){
@@ -39,28 +45,27 @@ public class ProcesadorJson {
 
         try (FileReader reader = new FileReader(file)) {
             // Deserializar el JSON a una lista de objetos Pokemon, decimos cómo es el tipo de lista
+            //Pokemon[] a=new Pokemon[1];
             Type streamPokemon = new TypeToken<List<Pokemon>>(){}.getType();
             // Leemos el JSON y lo convertimos a una lista de objetos Pokemon una vez definido el tipo
-            pokedex = gson.fromJson(reader, streamPokemon);
+            pokedex.setPokemon(gson.fromJson(reader, streamPokemon));
+            System.out.println("aa");
+
         } catch (IOException e) {
             System.out.println("aa");
             e.printStackTrace();
         }
     }
-/*
+
     List <Pokemon> forEach(Function<Pokemon,Boolean> fun){
-        List<Pokemon> result=new ArrayList<>();
-        getPokedex().getPokemon().stream().forEach(e->{
-            if (fun.apply(e))
-                result.add(e);
-        });
-        return result;
+        return new ArrayList<>(getPokedex().getPokemon().stream().filter(fun::apply).toList());
     }
-*/
+
     public static void main(String[] args) {
         ProcesadorJson pj=new ProcesadorJson();
         //System.out.println(pj.getPokedex().getPokemon().get(1).toString());
-        //List<Pokemon> res=pj.forEach(e->e.getName().equals("Pikachu"));
+        List<Pokemon> res=pj.forEach(e->e.getName().equals("Pikachu"));
+        System.out.println(res.get(0).toString());
         //res.forEach(e->System.out.println(e.toString()));
     }
 }
